50581367ead5f469bac7c3771edc71add11ffc04
namnhatpham1995 <namnhatpham1995@gmail.com>
2022-08-05 09:21:32 +0200

    Add help option, change argument check method

--- Create-or-delete-ingress-rule-with-public-IP-to-AWS-security-group.cmd
+++ Create-or-delete-ingress-rule-with-public-IP-to-AWS-security-group.cmd
@@ -2,10 +2,31 @@
 :: set all the variables in this script locally to make sure they are cleared after finishing
 setlocal
 
+:: Option arguments check
+if not [%1]==[] (
+	:: End the script if there is argument -d or --delete
+	for %%x in (%*) do (
+		IF %%~x == -d (
+			set "DELETE_MODE=true"
+		)
+		IF %%~x == --delete (
+			set "DELETE_MODE=true"
+		)
+		IF %%~x == --help (
+			goto :HELP
+		)
+		IF %%~x == -h (
+			goto :HELP
+		)
+	)
+)
+
 set SECURITY_GROUP_ID=sg-06c04c10fc3a5cdb4
 
+:: Set CURRENT_PUBLIC_IP variable
 for /F %%I in ('curl -ssS https://checkip.amazonaws.com') do set CURRENT_PUBLIC_IP=%%I
 
+:: Set AWS_USERNAME variable
 for /F %%I in ('aws iam get-user --output text --query "User.UserName"') do set AWS_USERNAME=%%I 
 set "SG_DESCRIPTION=%AWS_USERNAME%-dev-machine"
 :: remove space between words
@@ -16,21 +37,9 @@ for /F %%I in ('aws ec2 describe-security-groups --filters "Name=group-id,Values
 IF not "%OLD_PUBLIC_IP%"=="" (
 	call :DELETE_OLD_INGRESS_RULE
 )
-:: Option arguments check
-set argument_count=0
-for %%x in (%*) do Set /A argument_count+=1
-if %argument_count% GTR 0 (
-	:: End the script if there is argument -d or --delete
-	for %%x in (%*) do (
-		IF %%~x == -d (
-		:: End the script
-			goto :EOF
-		)
-		IF %%~x == --delete (
-		:: End the script
-			goto :EOF
-		)	
-	)
+
+if "%DELETE_MODE%" == "true" (
+    goto :EOF
 )
 
 call :CREATE_NEW_INGRESS_RULE
@@ -47,9 +56,9 @@ goto :EOF
 	EXIT /B
 	
 :CREATE_NEW_INGRESS_RULE
-	aws ec2 authorize-security-group-ingress 	^
-	--group-id %SECURITY_GROUP_ID% 				^
-	--ip-permissions "IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges=[{CidrIp=%CURRENT_PUBLIC_IP%/32,Description=%SG_DESCRIPTION%}]"
+	aws ec2 authorize-security-group-ingress 		^
+		--group-id %SECURITY_GROUP_ID% 				^
+		--ip-permissions "IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges=[{CidrIp=%CURRENT_PUBLIC_IP%/32,Description=%SG_DESCRIPTION%}]"
 	EXIT /B 0 	:: End the function
 
 :DELETE_OLD_INGRESS_RULE
@@ -59,3 +68,17 @@ goto :EOF
 		--port 22							^
 		--cidr %OLD_PUBLIC_IP%
 	EXIT /B 0	:: End the function
+
+:HELP
+	echo.
+	echo "This script is used to clear OLD ingress rules (containing old IP address(es))"
+	echo "and add the NEW  ingress rule(s) (containing new IP address(es))"
+	echo "to a determined AWS security group."
+	echo.
+	echo "Syntax: PI1-team-set-ingress-rule-with-public-IP-to-AWS-security-group.cmd [option(s)]"
+	echo.
+	echo "OPTIONS:"
+	echo "-h | --help             get list of commands"
+	echo "-d | --delete           only delete the old ingress rule without adding new one" 
+	echo.
+	EXIT /B
